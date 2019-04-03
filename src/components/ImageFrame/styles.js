04d89fe1169f93fa07dbaac93d87ab33d551ed0f
nusirwan <inbox@iwan.im>
2019-04-04 01:50:06 +0700

    Create Component ImageFrame

---
+++ src/components/ImageFrame/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+	width: 100%;
+	height: 100%;
+`;
+Wrap.displayName = "Wrap";
+
+export const Image = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	object-position: center;
+	vertical-align: middle;
+`;
+Image.displayName = "Image";

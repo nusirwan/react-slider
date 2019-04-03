1a5234b204f581b942c110c91c47782cfa698a93
nusirwan <inbox@iwan.im>
2019-04-03 22:01:06 +0700

    Import & Use Fragment & Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import Container from '../src/components/Container';
 
 export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<p>React Slider</p>
-			</div>
+			<Fragment>
+				<Container>
+					<p>React Slider</p>
+				</Container>
+			</Fragment>
 		);
 	}
 }

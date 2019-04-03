757f3e3733588ff288a5fdc6bd370f72d7ff6647
nusirwan <inbox@iwan.im>
2019-04-04 01:21:54 +0700

    base-style: Add globalStyle

---
+++ src/base-styles/globalStyle.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		margin: 0;
+		border: 0;
+		padding: 0;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+	}
+
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	p {
+		margin: 0;
+		padding: 0;
+	}
+`
+export default GlobalStyle;

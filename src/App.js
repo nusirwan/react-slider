60bf77eccc8e5766f916a142c846505af33fb4a2
nusirwan <inbox@iwan.im>
2019-04-04 01:24:53 +0700

    App: Import & Use globalStyle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
 
+import GlobalStyle from '../src/base-styles/globalStyle';
 import Container from '../src/components/Container';
 
 export default class App extends Component {
 	render() {
 		return (
 			<Fragment>
+				<GlobalStyle />
 				<Container>
 					<p>React Slider</p>
 				</Container>

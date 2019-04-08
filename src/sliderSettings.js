9ecec899e40d1595cd0c5343d66c7e166a3605de
nusirwan <inbox@iwan.im>
2019-04-08 21:12:14 +0700

    sliderSettings: Modif & Cleanup

--- src/sliderSettings.js
+++ src/sliderSettings.js
@@ -1,30 +1,12 @@
 const settings = {
     infinite: true,
-    speed: 500,
+    speed: 600,
     arrows: false,
     rows: 2,
     slidesPerRow: 4,
     slidesToShow: 1,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
-        {
-            breakpoint: 824,
-            settings: {
-                rows: 1,
-                slidesPerRow: 1,
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
-        {
-            breakpoint: 769,
-            settings: {
-                rows: 2,
-                slidesPerRow: 1,
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        },
         {
             breakpoint: 767,
             settings: {
